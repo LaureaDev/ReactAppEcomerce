012d165c6b3d6bac6fe88c1e3639f3e527b9ec6f
LaureanoNatalutti <nataluttilaureano@gmail.com>
2022-05-16 20:32:54 -0300

    NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css'
+const NavBar = () => {
+    return (
+<nav>
+            <section class="contenedor nav">
+                <div class="enlaces-header">
+                    <a href="">Inicio</a>
+                    <a href="">Aprende más</a>
+                    <a href="">Galeria</a>
+                    <a href="" target="blank">Login</a>
+                </div>
+            </section>
+        </nav>  
+    )
+}
+
+export default NavBar
