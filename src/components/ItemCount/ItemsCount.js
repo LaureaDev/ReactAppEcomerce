f9841d9a73eb2f3d039b6a9f00af47f74dc10216
LaureanoNatalutti <nataluttilaureano@gmail.com>
2022-05-30 22:27:23 -0300

    Modificaciones de Boton

--- src/components/ItemCount/ItemsCount.js
+++ src/components/ItemCount/ItemsCount.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import Button from 'react-bootstrap/button'
+import Button from 'react-bootstrap/Button'
 import Alert from 'react-bootstrap/Alert'
 function ItemCount({stock}) {
     const [count, setCount] = useState(0);
